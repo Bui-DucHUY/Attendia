f6c340ab73f4568f6e685aa92101361afffff856
Bùi Đức Huy <buiduchuy2606@gmail.com>
2026-04-20 03:42:59 -0400

    Update app.routes.ts

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,22 +1,21 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login';
-import { RegisterComponent } from './components/register/register';
-import { DashboardComponent } from './components/dashboard/dashboard';
-import { SessionManagerComponent } from './components/session-manager/session-manager';
-import { StudentCheckinComponent } from './components/student-checkin/student-checkin';
-import { authGuard } from './guards/auth.guard'; // We will create this next
-
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { Dashboard } from './components/dashboard/dashboard';
+import { SessionManager } from './components/session-manager/session-manager';
+import { StudentCheckin } from './components/student-checkin/student-checkin';
+import { authGuard } from './guards/auth-guard';
 export const routes: Routes = [
   // Public Routes
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
   
   // Mobile Student Route (Public, but requires specific Session ID)
-  { path: 'checkin/:sessionId', component: StudentCheckinComponent },
+  { path: 'checkin/:sessionId', component: StudentCheckin },
 
   // Protected Instructor Routes
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: 'session/:crn', component: SessionManagerComponent, canActivate: [authGuard] },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: 'session/:crn', component: SessionManager, canActivate: [authGuard] },
 
   // Default Fallbacks
   { path: '', redirectTo: 'login', pathMatch: 'full' },
